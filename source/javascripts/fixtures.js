569e551a8753b3215194e5bd63086c9614193761
Juan C. Müller <jcmuller@gmail.com>
2013-04-27 23:08:09 -0400

    D3

---
+++ source/javascripts/fixtures.js
@@ -0,0 +1,61 @@
+;(function() {
+  window.DATA = [
+    {
+      'name': 'Foursquare',
+      'url': 'http://foursquare.com',
+      'category': 'social',
+      'techStack': ['objc', 'rails', 'jquery', 'mysql'],
+      'launchDate': '2009-03',
+      'fundingEvents': [
+        {
+          'type': 'angel',
+          'date': '2009-09',
+          'amount': 1.3
+        },
+        {
+          'type': 'series-b',
+          'date': '2010-06',
+          'amount': 20
+        },
+        {
+          'type': 'series-c',
+          'date': '2011-06',
+          'amount': 50
+        },
+        {
+          'type': 'series-d',
+          'date': '2012-04',
+          'amount': 41
+        }
+      ],
+      'score': 0.5,
+      'logoUrl': 'https://ss1.4sqi.net/img/chrome/logo@2x-a49810b71b04213ca1ec0b41d2f1ff34.png'
+    },
+    {
+      'name': 'OK Cupid',
+      'url': 'http://okcupid.com',
+      'category': 'dating',
+      'platforms': [
+        {
+          'name': 'web',
+          'techStack': ['django', 'jquery', 'mysql']
+        }
+      ],
+      'launchDate': '2004-03',
+      'fundingEvents': [
+        {
+          'type': 'series-a',
+          'date': '2007-01',
+          'amount': 6
+        },
+        {
+          'type': 'acquired',
+          'date': '2011-02',
+          'amount': 50
+        }
+      ],
+      'score': 0.5,
+      'logoUrl': 'http://akcdn.okccdn.com/media/img/template/2012/logo.png'
+    }
+  ];
+})();
